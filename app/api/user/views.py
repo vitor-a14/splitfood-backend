825510e479685667a03fecc9cd2570cfd931f9d1
Vitor Silva Bueno <vitot770@gmail.com>
2024-04-01 21:47:51 -0300

    feat: added password check and cpf clean

--- app/api/user/views.py
+++ app/api/user/views.py
@@ -6,6 +6,7 @@ from fastapi import APIRouter, Depends, status, HTTPException
 
 from .schemas import UserSchema
 import validate_cpf
+import re
 
 router = APIRouter()
 
@@ -14,8 +15,15 @@ router = APIRouter()
              status_code=status.HTTP_201_CREATED)
 def create(user: UserSchema,
            repository: UserRepository = Depends()):
+
+    user.cpf = user.cpf.replace(".", "").replace("-", "")
+
     if not validate_cpf.is_valid(user.cpf):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid CPF")
+
+    if not re.match(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$', user.password):
+        print(user.password)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid password")
     
     return repository.create(User(**user.dict()))
 
